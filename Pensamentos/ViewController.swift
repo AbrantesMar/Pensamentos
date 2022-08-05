3c48d925d082fc00c18f421e21ee98311b3b8c80
Márcio Abrantes <abs.marcio@gmail.com>
2022-08-04 23:30:37 -0300

    Initial Commit

---
+++ Pensamentos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by Marcio Henrique Nunes Abrantes on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
